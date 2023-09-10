5c01c0493f62ca0e4e3ada83fe03877bf169a550
ryan-bajzath-k1 <r.bajzath@hotmail.com>
2023-09-10 14:29:44 -0400

    Started the blog portion and added an API to emulate getting data from the backend

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import Blog from "../components/Blogs";
+
+//Importing styling and Icons
+import styled from "styled-components";
+
+
 function LandingPage() {
-  return <h1>Landing</h1>;
+  return (<>
+  <StyledHeader>Welcome to my Blog</StyledHeader>
+  <Blog/>
+  </>)
 }
+
+const StyledHeader = styled.h1`
+text-align:center;
+margin-top:10px;`
+
 export default LandingPage;
