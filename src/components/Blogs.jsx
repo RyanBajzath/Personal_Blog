634c7837dd446c642d20b91d7c7384106ac0694a
Ryan Bajzath <R.bajzath@hotmail.com>
2023-09-18 23:52:42 -0400

    test

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,28 +1,28 @@
 //Importing React and States
 import React from "react";
-import { useEffect, useState} from "react";
-
+import { useEffect, useState } from "react";
 
 // using an API to create blog posts to emulate getting data from the back end.
-function Blog(){
-    const[blog, setBlog] = useState("")
+function Blog() {
+  const [blog, setBlog] = useState("");
 
-    //This hook will activate on mount only to prevent a loop, it fetches the data then stores it as a state.
-    useEffect(() => {
-        fetch( 'https://baconipsum.com/api/?callback=?')
-          .then((res) => res.json())
-          .then((data) => {
-           console.log(data)
-           setBlog(data)
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }, []);
-   return (<>
-   <h1>blog</h1>
-   <h2>{blog}</h2>
-   </>
-   )
+  //This hook will activate on mount only to prevent a loop, it fetches the data then stores it as a state.
+  useEffect(() => {
+    fetch("https://baconipsum.com/api/?callback=?")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBlog(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <>
+      <h1>blog test</h1>
+      <h2>{blog}</h2>
+    </>
+  );
 }
 export default Blog;
