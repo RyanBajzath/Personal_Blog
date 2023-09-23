9878de2230b0241f70650c0bb64dc15e8d58becb
Ryan Bajzath <R.bajzath@hotmail.com>
2023-09-22 20:25:47 -0400

    Added my 1st blog

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,6 +1,7 @@
 //Importing React and States
 import React from "react";
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 
 // using an API to create blog posts to emulate getting data from the back end.
 function Blog() {
@@ -20,9 +21,50 @@ function Blog() {
   }, []);
   return (
     <>
-      <h1>blog test</h1>
-      <h2>{blog}</h2>
+      <p>
+        <b>Blog,</b>
+        <i>noun.</i>a regularly updated website or web page, typically one run
+        by an individual or small group, that is written in an informal or
+        conversational style.
+      </p>
+      <StyledBlogContainer>
+        <SyledBlog>
+          <h1>First blog, got a job</h1>
+          <StyledP>
+            I have experimented with different careers, schools, side projects
+            and relaized, I wanted a craft that combines my artistic and logical
+            passions. My coding journey started in 2016 with little python
+            projects. I found the idea of creating something and someone else
+            being able to interact with it facinating. The means of distribution
+            is also relativly easier, a website rather then creating something
+            physical. I eventually completed a full stack developer course and I
+            will be working as a front end developper for the 1st time next
+            Monday the 25 of September. I am nervous but also happy to try, if I
+            keep trying and learning then there is growth which is it's own
+            reward.
+          </StyledP>
+        </SyledBlog>
+      </StyledBlogContainer>
     </>
   );
 }
+
+const StyledBlogContainer = styled.div`
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  margin-top: 20px;
+`;
+
+const SyledBlog = styled.div`
+  /* Remove display: flex; from here */
+
+  /* Other styles remain the same */
+
+  border: 5px solid black; /* Apply border to the container */
+  width: 80vw;
+  padding: 10px;
+`;
+
+const StyledP = styled.p``;
 export default Blog;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,16 +4,21 @@ import Blog from "../components/Blogs";
 //Importing styling and Icons
 import styled from "styled-components";
 
-
 function LandingPage() {
-  return (<>
-  <StyledHeader>Welcome to my Blog</StyledHeader>
-  <Blog/>
-  </>)
+  return (
+    <>
+      <PageDiv>
+        <StyledHeader>Welcome to my Blog</StyledHeader>
+        <Blog />
+      </PageDiv>
+    </>
+  );
 }
 
 const StyledHeader = styled.h1`
-text-align:center;
-margin-top:10px;`
+  text-align: center;
+`;
+
+const PageDiv = styled.div``;
 
 export default LandingPage;
